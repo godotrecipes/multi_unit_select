7aa696850b56a80d7e1007fdda69a262d8c57bc1
Chris Bradfield <cb@scribe.net>
2023-07-09 11:34:02 -0700

    update to 4.1

--- unit.gd
+++ unit.gd
@@ -10,23 +10,26 @@ var target = null:
 	set = set_target
 
 func _ready():
-	print(transform)
-	# Duplicate the mater
+	# Make each unit's material unique
 	$Sprite2D.material = $Sprite2D.material.duplicate()
 	
 func _physics_process(delta):
 	velocity = Vector2.ZERO
 	if target:
+		# Move toward the target
 		velocity = position.direction_to(target)
+		# Stop moving when reaching the target
 		if position.distance_to(target) < target_radius:
 			target = null
 	av = avoid()
+	# Add an avoidance vector to move away from other units
 	velocity = (velocity + av * avoid_weight).normalized() * speed
 	if velocity.length() > 0:
 		rotation = velocity.angle()
 	move_and_collide(velocity * delta)
 	
 func set_selected(value):
+	# Toggle the outline
 	selected = value
 	if selected:
 		$Sprite2D.material.set_shader_parameter("aura_width", 1)
@@ -37,6 +40,7 @@ func set_target(value):
 	target = value
 	
 func avoid():
+	# Finds a vector that points away from other units
 	var result = Vector2.ZERO
 	var neighbors = $Detect.get_overlapping_bodies()
 	if neighbors:
